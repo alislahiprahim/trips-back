ff73b093dd51fca2d114ae5feffd496d3b4009f6
alislahiprahim <alislahiprahim99@gmail.com>
2022-07-23 19:50:01 +0200

    heroku second build

--- server.js
+++ server.js
@@ -1,49 +1,54 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const mongoose = require("mongoose")
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const app = express();
 const cors = require("cors");
-const server = require('http').createServer(app)
-app.use(bodyParser.json())
-var router = express.Router()
-var VerifyToken = require("./middelWare/VerifyToken")
+const server = require("http").createServer(app);
+app.use(bodyParser.json());
+var router = express.Router();
+var VerifyToken = require("./middelWare/VerifyToken");
 
-mongoose.connect("mongodb://localhost:27017/Traveling")
-mongoose.connection.on("error", err => {
+mongoose.connect(
+    "mongodb+srv://yalla_bana_database:yalla_beana_pass_1234@cluster0.kk6ddve.mongodb.net/?retryWrites=true&w=majority"
+);
+mongoose.connection.on("error", (err) => {
     console.error(`MongoDB connection error: ${err}`);
     process.exit(1);
 });
 
 app.use(
     cors({
-        origin: ' http://localhost:4200',
+        origin: " http://localhost:4200",
         credentials: true,
-        maxAge: 1000000
+        maxAge: 1000000,
     })
+);
 
-)
+app.get("/", (req, resp) => {
+    const {} = req.body;
+    resp.send(
+        JSON.stringify({
+            status: true,
+            message: "Server Is Running",
+            data: "No data",
+        })
+    );
+});
+var User = require("./controllers/userAPIs");
+var Trip = require("./controllers/tripAPIs");
+var Login = require("./controllers/Login");
 
-app.get('/', (req, resp) => {
+app.use("/user", User);
+app.use("/trip", Trip);
+app.use("/login", Login);
 
-    const {} = req.body
-    resp.send(JSON.stringify({ status: true, message: 'Server Is Running', data: 'No data' }))
-
-})
-var User = require("./controllers/userAPIs")
-var Trip = require("./controllers/tripAPIs")
-var Login = require("./controllers/Login")
-
-app.use("/user", User)
-app.use("/trip", Trip)
-app.use('/login', Login)
-
-app.get('/resources/images/:fileName', VerifyToken, (req, resp) => {
-    const { fileName } = req.params
-    var __dirname = "./resources/images/"
+app.get("/resources/images/:fileName", VerifyToken, (req, resp) => {
+    const { fileName } = req.params;
+    var __dirname = "./resources/images/";
     resp.sendFile(fileName, { root: __dirname }, (err, data) => {
-        console.log("err", err)
-        console.log("data", data)
-    })
-})
+        console.log("err", err);
+        console.log("data", data);
+    });
+});
 
-server.listen(8085)
+server.listen(8085);
